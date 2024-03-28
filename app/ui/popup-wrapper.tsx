bc163f9d0eba4cc2e909c7f79c351de00610de21
Roh Jaehee <roh-j@naver.com>
2024-03-28 14:06:05 +0900

    팝업 제어 로직 변경

--- app/ui/popup-wrapper.tsx
+++ app/ui/popup-wrapper.tsx
@@ -2,8 +2,18 @@ import Popup from "./popup";
 import { pb } from "../shared/connection";
 import { Suspense } from "react";
 
+export interface PopupResponse {
+  active: boolean;
+  collectionId: string;
+  collectionName: string;
+  created: Date;
+  id: string;
+  image: string;
+  updated: Date;
+}
+
 const PopupWrapper = async () => {
-  const data = await pb
+  const data: PopupResponse[] = await pb
     .collection("popups")
     .getFullList({ filter: "active = true" });
 
--- app/ui/popup.tsx
+++ app/ui/popup.tsx
@@ -1,60 +1,109 @@
 "use client";
 
+import Cookies from "js-cookie";
 import Draggable from "react-draggable";
 import Image from "next/image";
 import styles from "./popup.module.css";
 import { POCKET_BASE_URL } from "../constants/connection";
+import { PopupResponse } from "./popup-wrapper";
 import { useEffect, useState } from "react";
 
-const Popup = (props: any) => {
+interface PopupsState extends PopupResponse {
+  isVisible: boolean;
+}
+
+interface PopupProps {
+  data: PopupResponse[];
+}
+
+const Popup = (props: PopupProps) => {
   const { data } = props;
 
-  const date = new Date();
-
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDate();
-
-  const stamp = year + month + day;
-
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [popups, setPopups] = useState<PopupsState[]>(
+    data.map((row: PopupResponse) => ({
+      ...row,
+      isVisible: false,
+    }))
+  );
+  const [isReady, setIsReady] = useState<boolean>(false);
 
   useEffect(() => {
-    const popupStorage = localStorage.getItem("popup");
+    let newPopups = [...popups];
 
-    if (popupStorage && Number(popupStorage) < stamp) {
-      localStorage.removeItem("popup");
-      return;
+    // Cookie가 존재하지 않는다면 팝업 열기
+    for (let i = 0; i < newPopups.length; i++) {
+      if (!Cookies.get(`popup_${newPopups[i].id}`)) {
+        newPopups[i] = {
+          ...newPopups[i],
+          isVisible: true,
+        };
+      }
     }
 
-    setIsVisible(true);
+    setPopups(newPopups);
+    setIsReady(true);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const closeToday = () => {
-    localStorage.setItem("popup", String(stamp));
+  const closeToday = (id: string) => {
+    setPopups(
+      popups.map((row: PopupsState) => {
+        if (row.id === id) {
+          return {
+            ...row,
+            isVisible: false,
+          };
+        }
+
+        return row;
+      })
+    );
+
+    const today = new Date();
+    const expires = new Date(
+      Date.UTC(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate(),
+        23,
+        59,
+        59
+      )
+    );
+
+    Cookies.set(`popup_${id}`, "1", {
+      expires,
+    });
   };
 
   return (
     <>
-      {isVisible && (
+      {isReady && (
         <>
-          {data.map((row: any, index: number) => (
-            <Draggable key={index} positionOffset={{ x: "-50%", y: "-50%" }}>
-              <div className={styles.container}>
-                <Image
-                  src={`${POCKET_BASE_URL}/api/files/${row.collectionName}/${row.id}/${row.image}`}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: "auto", height: "100%" }}
-                  alt=""
-                />
-                <div onClick={closeToday}>오늘 하루동안 열지 않기</div>
-              </div>
-            </Draggable>
-          ))}
+          {popups
+            .filter((row: PopupsState) => row.isVisible)
+            .map((row: PopupsState, index: number) => (
+              <Draggable key={index} positionOffset={{ x: "-50%", y: "-50%" }}>
+                <div className={styles.container}>
+                  <Image
+                    src={`${POCKET_BASE_URL}/api/files/${row.collectionName}/${row.id}/${row.image}`}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: "auto", height: "100%" }}
+                    alt=""
+                  />
+                  <div
+                    onClick={() => {
+                      closeToday(row.id);
+                    }}
+                  >
+                    오늘 하루동안 열지 않기
+                  </div>
+                </div>
+              </Draggable>
+            ))}
         </>
       )}
     </>
