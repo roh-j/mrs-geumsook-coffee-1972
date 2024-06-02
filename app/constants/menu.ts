96cbd63695d414f7317a8633f2bb8faade33bf98
Roh Jaehee <roh-j@naver.com>
2024-06-03 03:29:27 +0900

    이미지 파일명 공백 제거

--- app/constants/menu.ts
+++ app/constants/menu.ts
@@ -36,22 +36,22 @@ const MENU_CATEGORY = [
 const MENU_ITEMS = [
   {
     name: "1000ml 보틀라떼&에이드",
-    src: "/menu/1l-large-capacity/1000ml 보틀라떼&에이드.jpg",
+    src: "/menu/1l-large-capacity/1000ml_보틀라떼&에이드.jpg",
     category: "1L_LARGE_CAPACITY",
   },
   {
     name: "1000ml 보틀커피",
-    src: "/menu/1l-large-capacity/1000ml 보틀커피.jpg",
+    src: "/menu/1l-large-capacity/1000ml_보틀커피.jpg",
     category: "1L_LARGE_CAPACITY",
   },
   {
     name: "1000ml 아이스티&티",
-    src: "/menu/1l-large-capacity/1000ml 아이스티&티.jpg",
+    src: "/menu/1l-large-capacity/1000ml_아이스티&티.jpg",
     category: "1L_LARGE_CAPACITY",
   },
   {
     name: "1000ml 쥬스&스무디",
-    src: "/menu/1l-large-capacity/1000ml 쥬스&스무디.jpg",
+    src: "/menu/1l-large-capacity/1000ml_쥬스&스무디.jpg",
     category: "1L_LARGE_CAPACITY",
   },
   {
@@ -86,7 +86,7 @@ const MENU_ITEMS = [
   },
   {
     name: "패션후르츠 에이드",
-    src: "/menu/ade/패션후르츠 에이드.jpg",
+    src: "/menu/ade/패션후르츠_에이드.jpg",
     category: "ADE",
   },
   {
@@ -96,22 +96,22 @@ const MENU_ITEMS = [
   },
   {
     name: "매실 아이스티",
-    src: "/menu/aromatic-tea/매실 아이스티.jpg",
+    src: "/menu/aromatic-tea/매실_아이스티.jpg",
     category: "AROMATIC_TEA",
   },
   {
     name: "복분자 아이스티",
-    src: "/menu/aromatic-tea/복분자 아이스티.jpg",
+    src: "/menu/aromatic-tea/복분자_아이스티.jpg",
     category: "AROMATIC_TEA",
   },
   {
     name: "복숭아 아이스티",
-    src: "/menu/aromatic-tea/복숭아 아이스티.jpg",
+    src: "/menu/aromatic-tea/복숭아_아이스티.jpg",
     category: "AROMATIC_TEA",
   },
   {
     name: "석류 아이스티",
-    src: "/menu/aromatic-tea/석류 아이스티.jpg",
+    src: "/menu/aromatic-tea/석류_아이스티.jpg",
     category: "AROMATIC_TEA",
   },
   {
@@ -196,22 +196,22 @@ const MENU_ITEMS = [
   },
   {
     name: "헤이즐넛 카페라떼",
-    src: "/menu/coffee/헤이즐넛 카페라떼.jpg",
+    src: "/menu/coffee/헤이즐넛_카페라떼.jpg",
     category: "COFFEE",
   },
   {
     name: "[콜드브루] 아메리카노",
-    src: "/menu/cold-brew/[콜드브루] 아메리카노.jpg",
+    src: "/menu/cold-brew/[콜드브루]_아메리카노.jpg",
     category: "COLD_BREW",
   },
   {
     name: "[콜드브루] 연유라떼",
-    src: "/menu/cold-brew/[콜드브루] 연유라떼.jpg",
+    src: "/menu/cold-brew/[콜드브루]_연유라떼.jpg",
     category: "COLD_BREW",
   },
   {
     name: "[콜드브루] 카페라떼",
-    src: "/menu/cold-brew/[콜드브루] 카페라떼.jpg",
+    src: "/menu/cold-brew/[콜드브루]_카페라떼.jpg",
     category: "COLD_BREW",
   },
   {
@@ -226,12 +226,12 @@ const MENU_ITEMS = [
   },
   {
     name: "금숙씨네 치즈가득 크로크무슈",
-    src: "/menu/dessert/금숙씨네 치즈가득 크로크무슈.jpg",
+    src: "/menu/dessert/금숙씨네_치즈가득_크로크무슈.jpg",
     category: "DESSERT",
   },
   {
     name: "누텔라 바나나 와플",
-    src: "/menu/dessert/누텔라 바나나 와플.jpg",
+    src: "/menu/dessert/누텔라_바나나_와플.jpg",
     category: "DESSERT",
   },
   {
@@ -241,22 +241,22 @@ const MENU_ITEMS = [
   },
   {
     name: "먹기좋은 한입과일",
-    src: "/menu/dessert/먹기좋은 한입과일.jpg",
+    src: "/menu/dessert/먹기좋은_한입과일.jpg",
     category: "DESSERT",
   },
   {
     name: "생과일 딸기 와플",
-    src: "/menu/dessert/생과일 딸기 와플.jpg",
+    src: "/menu/dessert/생과일_딸기_와플.jpg",
     category: "DESSERT",
   },
   {
     name: "생크림 와플",
-    src: "/menu/dessert/생크림 와플.jpg",
+    src: "/menu/dessert/생크림_와플.jpg",
     category: "DESSERT",
   },
   {
     name: "생크림 크로아상",
-    src: "/menu/dessert/생크림 크로아상.jpg",
+    src: "/menu/dessert/생크림_크로아상.jpg",
     category: "DESSERT",
   },
   {
@@ -266,7 +266,7 @@ const MENU_ITEMS = [
   },
   {
     name: "스테이바 감자",
-    src: "/menu/dessert/스테이바 감자.jpg",
+    src: "/menu/dessert/스테이바_감자.jpg",
     category: "DESSERT",
   },
   {
@@ -276,7 +276,7 @@ const MENU_ITEMS = [
   },
   {
     name: "애플 시나몬 와플",
-    src: "/menu/dessert/애플 시나몬 와플.jpg",
+    src: "/menu/dessert/애플_시나몬_와플.jpg",
     category: "DESSERT",
   },
   {
@@ -296,17 +296,17 @@ const MENU_ITEMS = [
   },
   {
     name: "초코 와플",
-    src: "/menu/dessert/초코 와플.jpg",
+    src: "/menu/dessert/초코_와플.jpg",
     category: "DESSERT",
   },
   {
     name: "카야버터 토스트",
-    src: "/menu/dessert/카야버터 토스트.jpg",
+    src: "/menu/dessert/카야버터_토스트.jpg",
     category: "DESSERT",
   },
   {
     name: "크로플 X 네가지맛",
-    src: "/menu/dessert/크로플 X 네가지맛.jpg",
+    src: "/menu/dessert/크로플X네가지맛.jpg",
     category: "DESSERT",
   },
   {
@@ -336,12 +336,12 @@ const MENU_ITEMS = [
   },
   {
     name: "딸기 바나나 쉐이크",
-    src: "/menu/fresh-fruit-juice/딸기 바나나 쉐이크.jpg",
+    src: "/menu/fresh-fruit-juice/딸기_바나나_쉐이크.jpg",
     category: "FRESH_FRUIT_JUICE",
   },
   {
     name: "바나나 밀크쉐이크",
-    src: "/menu/fresh-fruit-juice/바나나 밀크쉐이크.jpg",
+    src: "/menu/fresh-fruit-juice/바나나_밀크쉐이크.jpg",
     category: "FRESH_FRUIT_JUICE",
   },
   {
@@ -376,17 +376,17 @@ const MENU_ITEMS = [
   },
   {
     name: "새우 볶음밥",
-    src: "/menu/fried-rice/새우 볶음밥.jpg",
+    src: "/menu/fried-rice/새우_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
     name: "소불고기 볶음밥",
-    src: "/menu/fried-rice/소불고기 볶음밥.jpg",
+    src: "/menu/fried-rice/소불고기_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
     name: "신라면 볶음밥",
-    src: "/menu/fried-rice/신라면 볶음밥.jpg",
+    src: "/menu/fried-rice/신라면_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
@@ -396,22 +396,22 @@ const MENU_ITEMS = [
   },
   {
     name: "오징어짬뽕 볶음밥",
-    src: "/menu/fried-rice/오징어짬뽕 볶음밥.jpg",
+    src: "/menu/fried-rice/오징어짬뽕_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
     name: "짜장범벅 볶음밥",
-    src: "/menu/fried-rice/짜장범벅 볶음밥.jpg",
+    src: "/menu/fried-rice/짜장범벅_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
     name: "참깨라면 볶음밥",
-    src: "/menu/fried-rice/참깨라면 볶음밥.jpg",
+    src: "/menu/fried-rice/참깨라면_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
     name: "햄야채 볶음밥",
-    src: "/menu/fried-rice/햄야채 볶음밥.jpg",
+    src: "/menu/fried-rice/햄야채_볶음밥.jpg",
     category: "FRIED_RICE",
   },
   {
@@ -471,7 +471,7 @@ const MENU_ITEMS = [
   },
   {
     name: "옹심이 팥죽",
-    src: "/menu/porridge/옹심이 팥죽.jpg",
+    src: "/menu/porridge/옹심이_팥죽.jpg",
     category: "PORRIDGE",
   },
   {
@@ -481,77 +481,72 @@ const MENU_ITEMS = [
   },
   {
     name: "딸기 요거트스무디",
-    src: "/menu/smoothie/딸기 요거트스무디.jpg",
+    src: "/menu/smoothie/딸기_요거트스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "망고 요거트스무디",
-    src: "/menu/smoothie/망고 요거트스무디.jpg",
+    src: "/menu/smoothie/망고_요거트스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "망고스무디",
-    src: "/menu/smoothie/망고스무디.jpg",
-    category: "SMOOTHIE",
-  },
-  {
-    name: "망고요거트 스무디",
-    src: "/menu/smoothie/망고요거트 스무디.jpg",
+    src: "/menu/smoothie/망고_스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "미숫가루 쉐이크",
-    src: "/menu/smoothie/미숫가루 쉐이크.jpg",
+    src: "/menu/smoothie/미숫가루_쉐이크.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "복숭아 요거트스무디",
-    src: "/menu/smoothie/복숭아 요거트스무디.jpg",
+    src: "/menu/smoothie/복숭아_요거트스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "블루베리 스무디",
-    src: "/menu/smoothie/블루베리 스무디.jpg",
+    src: "/menu/smoothie/블루베리_스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "블루베리 요거트 스무디",
-    src: "/menu/smoothie/블루베리 요거트 스무디.jpg",
+    src: "/menu/smoothie/블루베리_요거트스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "빨기바나나 스무디",
-    src: "/menu/smoothie/빨기바나나 스무디.jpg",
+    src: "/menu/smoothie/빨기바나나_스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "스트로베리 피치 스무디",
-    src: "/menu/smoothie/스트로베리 피치 스무디.jpg",
+    src: "/menu/smoothie/스트로베리_피치_스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "오렌지 자몽 스무디",
-    src: "/menu/smoothie/오렌지 자몽 스무디.jpg",
+    src: "/menu/smoothie/오렌지_자몽_스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "초코초코 쉐이크",
-    src: "/menu/smoothie/초코초코 쉐이크.jpg",
+    src: "/menu/smoothie/초코초코_쉐이크.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "플레인 요거트스무디",
-    src: "/menu/smoothie/플레인 요거트스무디.jpg",
+    src: "/menu/smoothie/플레인_요거트스무디.jpg",
     category: "SMOOTHIE",
   },
   {
     name: "고구마 튀김",
-    src: "/menu/snacks/고구마 튀김.jpg",
+    src: "/menu/snacks/고구마_튀김.jpg",
     category: "SNACKS",
   },
   {
     name: "금숙씨네 뜨끈한 구운가래떡",
-    src: "/menu/snacks/금숙씨네 뜨끈한 구운가래떡.jpg",
+    src: "/menu/snacks/금숙씨네_뜨끈한_구운가래떡.jpg",
     category: "SNACKS",
   },
   {
@@ -561,87 +556,87 @@ const MENU_ITEMS = [
   },
   {
     name: "매운매운 타코야끼",
-    src: "/menu/snacks/매운매운 타코야끼.jpg",
+    src: "/menu/snacks/매운매운_타코야끼.jpg",
     category: "SNACKS",
   },
   {
     name: "매콤매콤 핫도그으",
-    src: "/menu/snacks/매콤매콤 핫도그으.jpg",
+    src: "/menu/snacks/매콤매콤_핫도그으.jpg",
     category: "SNACKS",
   },
   {
     name: "미니 물냉면",
-    src: "/menu/snacks/미니 물냉면.jpg",
+    src: "/menu/snacks/미니_물냉면.jpg",
     category: "SNACKS",
   },
   {
     name: "미니 비빔냉면",
-    src: "/menu/snacks/미니 비빔냉면.jpg",
+    src: "/menu/snacks/미니_비빔냉면.jpg",
     category: "SNACKS",
   },
   {
     name: "바삭한 미니 김말이 튀김",
-    src: "/menu/snacks/바삭한 미니 김말이 튀김.jpg",
+    src: "/menu/snacks/바삭한_미니_김말이_튀김.jpg",
     category: "SNACKS",
   },
   {
     name: "비빔 냉면",
-    src: "/menu/snacks/비빔 냉면.jpg",
+    src: "/menu/snacks/비빔_냉면.jpg",
     category: "SNACKS",
   },
   {
     name: "시즈닝X + 설탕만",
-    src: "/menu/snacks/시즈닝X + 설탕만.jpg",
+    src: "/menu/snacks/시즈닝X_+_설탕만.jpg",
     category: "SNACKS",
   },
   {
     name: "오리지널 타코야끼",
-    src: "/menu/snacks/오리지널 타코야끼.jpg",
+    src: "/menu/snacks/오리지널_타코야끼.jpg",
     category: "SNACKS",
   },
   {
     name: "오리지널 핫도그으",
-    src: "/menu/snacks/오리지널 핫도그으.jpg",
+    src: "/menu/snacks/오리지널_핫도그으.jpg",
     category: "SNACKS",
   },
   {
     name: "일반 물냉면",
-    src: "/menu/snacks/일반 물냉면.jpg",
+    src: "/menu/snacks/일반_물냉면.jpg",
     category: "SNACKS",
   },
   {
     name: "촉촉한 삶은 반숙계란",
-    src: "/menu/snacks/촉촉한 삶은 반숙계란.jpg",
+    src: "/menu/snacks/촉촉한_삶은_반숙계란.jpg",
     category: "SNACKS",
   },
   {
     name: "치즈 시즈닝 솔솔",
-    src: "/menu/snacks/치즈 시즈닝 솔솔.jpg",
+    src: "/menu/snacks/치즈_시즈닝_솔솔.jpg",
     category: "SNACKS",
   },
   {
     name: "치즈시즈닝 + 꿀",
-    src: "/menu/snacks/치즈시즈닝 + 꿀.jpg",
+    src: "/menu/snacks/치즈시즈닝_+_꿀.jpg",
     category: "SNACKS",
   },
   {
     name: "치즈으맛 핫도그으",
-    src: "/menu/snacks/치즈으맛 핫도그으.jpg",
+    src: "/menu/snacks/치즈으맛_핫도그으.jpg",
     category: "SNACKS",
   },
   {
     name: "치즈치즈 타코야끼",
-    src: "/menu/snacks/치즈치즈 타코야끼.jpg",
+    src: "/menu/snacks/치즈치즈_타코야끼.jpg",
     category: "SNACKS",
   },
   {
     name: "허니버터 시즈닝 + 꿀",
-    src: "/menu/snacks/허니버터 시즈닝 + 꿀.jpg",
+    src: "/menu/snacks/허니버터_시즈닝_+_꿀.jpg",
     category: "SNACKS",
   },
   {
     name: "허니버터 시즈닝 솔솔",
-    src: "/menu/snacks/허니버터 시즈닝 솔솔.jpg",
+    src: "/menu/snacks/허니버터_시즈닝_솔솔.jpg",
     category: "SNACKS",
   },
   {
@@ -656,7 +651,7 @@ const MENU_ITEMS = [
   },
   {
     name: "빙수 여러개",
-    src: "/menu/snowflake-bingsu/빙수 여러개.jpg",
+    src: "/menu/snowflake-bingsu/빙수_여러개.jpg",
     category: "SNOWFLAKE_BINGSU",
   },
   {
@@ -681,32 +676,32 @@ const MENU_ITEMS = [
   },
   {
     name: "로제 가래떡볶이",
-    src: "/menu/tteokbokki/로제 가래떡볶이.jpg",
+    src: "/menu/tteokbokki/로제_가래떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
   {
     name: "로제 떡볶이",
-    src: "/menu/tteokbokki/로제 떡볶이.jpg",
+    src: "/menu/tteokbokki/로제_떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
   {
     name: "일반 가래떡볶이",
-    src: "/menu/tteokbokki/일반 가래떡볶이.jpg",
+    src: "/menu/tteokbokki/일반_가래떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
   {
     name: "일반 떡볶이",
-    src: "/menu/tteokbokki/일반 떡볶이.jpg",
+    src: "/menu/tteokbokki/일반_떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
   {
     name: "짜장 가래떡볶이",
-    src: "/menu/tteokbokki/짜장 가래떡볶이.jpg",
+    src: "/menu/tteokbokki/짜장_가래떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
   {
     name: "짜장 떡볶이",
-    src: "/menu/tteokbokki/짜장 떡볶이.jpg",
+    src: "/menu/tteokbokki/짜장_떡볶이.jpg",
     category: "TTEOKBOKKI",
   },
 ];
