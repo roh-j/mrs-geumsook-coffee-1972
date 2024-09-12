0afa22425bb68fc354ea3cd132e46654f961d3b8
Roh Jaehee <roh-j@naver.com>
2024-09-12 20:20:01 +0900

    PocketBase 빌드 URL 수정

--- app/constants/connection.ts
+++ app/constants/connection.ts
@@ -1,3 +1,7 @@
-const POCKET_BASE_URL = "https://gsc1972.com:8090";
+// Production
+const POCKET_BASE_URL = "http://127.0.0.1:9090";
+
+// Development
+// const POCKET_BASE_URL = "https://gsc1972.com:8090";
 
 export { POCKET_BASE_URL };
