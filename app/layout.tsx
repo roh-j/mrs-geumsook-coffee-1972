b1f7419ef3d929db6d7e95f82b79a6a98776895c
Roh Jaehee <roh-j@naver.com>
2024-09-12 09:30:29 +0900

    사파리 브라우저 대응
    - 전화번호 인식 방지
    - 햄버거 아이콘 컬러 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         />
+        <meta name="format-detection" content="telephone=no" />
       </head>
       <body>{children}</body>
     </html>
