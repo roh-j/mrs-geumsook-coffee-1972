6b698ab06eff3d5c1f421282ba89dfa3e1f11fd8
Roh Jaehee <roh-j@naver.com>
2024-09-13 21:18:26 +0900

    Prop className did not match 오류 해결

--- app/ui/common.tsx
+++ app/ui/common.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import AOS from "aos";
 import { ToastContainer } from "react-toastify";
 import { useEffect } from "react";
 
@@ -12,10 +11,17 @@ const Common = () => {
     new PureCounter();
   };
 
-  useEffect(() => {
-    loadPureCounter();
+  // 'Prop className did not match' 오류를 해결하기 위해 동적 임포트를 적용.
+  // 동적 임포트를 사용하지 않으면 AOS로 인해 하이드레이션 시 className 불일치 발생.
+  const loadAOS = async () => {
+    const AOS = await import("aos");
 
     AOS.init({ offset: 200 });
+  };
+
+  useEffect(() => {
+    loadPureCounter();
+    loadAOS();
 
     window.history.scrollRestoration = "manual";
   }, []);
